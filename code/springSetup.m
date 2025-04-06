8dc9838b638b0da7fdc25a75cce96960e5e59e2b
aCodingTurtle <34428554+aCodingTurtle@users.noreply.github.com>
2025-04-06 15:38:16 -0400

    fixed unit conversions

--- code/springSetup.m
+++ code/springSetup.m
@@ -1,6 +1,7 @@
 %% LS3 Engine Valve Spring
 % This script will set up a spring struct and leave it in the workspace
 
-spring.K = 392 / 8.851; % [N/m] Spring Rate
-spring.Xpre = .001; % [m] Preload distance
-spring.stroke = .660 * .0254; % [m] Stroke Length (Max Lift)
+spring.K = 392 / .0254 * 4.44822; % [N/m] Spring Rate
+spring.Fpre = 160 * 4.44822; % [N] Preload Force (Seat Pressure)
+spring.Xpre = spring.Fpre / spring.K; % [m] Preload distance
+spring.stroke = .6 * .0254; % [m] Stroke Length (Max Lift)
