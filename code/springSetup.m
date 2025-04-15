2063cf3cb2ae90a85e7641ef82d53249e04c1303
aCodingTurtle <34428554+aCodingTurtle@users.noreply.github.com>
2025-04-15 13:03:41 -0400

    Updated spring stroke to .551in

--- code/springSetup.m
+++ code/springSetup.m
@@ -4,4 +4,4 @@
 spring.K = 392 / .0254 * 4.44822; % [N/m] Spring Rate
 spring.Fpre = 160 * 4.44822; % [N] Preload Force (Seat Pressure)
 spring.Xpre = spring.Fpre / spring.K; % [m] Preload distance
-spring.stroke = .6 * .0254; % [m] Stroke Length (Max Lift)
+spring.stroke = .551 * .0254; % [m] Stroke Length (Max Lift)
