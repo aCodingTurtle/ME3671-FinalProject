9e74f613ea1812cd20000dc34c71201df5694798
DW39645 <61071426+DW39645@users.noreply.github.com>
2025-04-19 15:31:22 -0400

    Update ShaftCalculations.m

--- code/ShaftCalculations.m
+++ code/ShaftCalculations.m
@@ -15,11 +15,11 @@ x_xx = 0;               %
 
 
 %% Shaft 1 (Motor and drive gear)
-F_dg = m_dg * g;        % Drive gear force
-F_m = F_dg / 2;         % Vertical motor force 
-F_b1 = F_dg / 2;        % Bearing force
-T_m = 0;                % Motor torque
-T_dg = 0;               % Drive gear torque on shaft
+F_dg = m_dg * g;        % Drive gear force (N)
+F_m = F_dg / 2;         % Vertical motor force (N) 
+F_b1 = F_dg / 2;        % Bearing force (N)
+T_m = 0;                % Motor torque (N*m)
+T_dg = 0;               % Drive gear torque on shaft (N*m)
 
 
 
@@ -27,12 +27,12 @@ T_dg = 0;               % Drive gear torque on shaft
 %% Shaft 2 (Driven gear, flywheel, coupling to crankshaft)
 F_pg = m_pg * g;        % Pinion gear force(N)
 F_fw = m_fw * g;        % Flywheel force (N)
-F_b2 = F_pg / 2;        % Bearing force 
-F_b3 = (F_pg+F_fw) / 2; % Bearing force 
-F_b4 = F_fw / 2;        % Bearing force 
-T_pg = 0;               % Pinion gear torque
-T_fw = 0;               % Flywheel torque
-T_cp = 0;               % Coupling torque
+F_b2 = F_pg / 2;        % Bearing force (N)
+F_b3 = (F_pg+F_fw) / 2; % Bearing force (N)
+F_b4 = F_fw / 2;        % Bearing force (N)
+T_pg = 0;               % Pinion gear torque (N*m)
+T_fw = 0;               % Flywheel torque (N*m)
+T_cp = 0;               % Coupling torque (N*m)
 
 
 
