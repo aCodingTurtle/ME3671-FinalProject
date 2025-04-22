fa6c004e34d99ce22712044f3453e2a3f3a519fa
Leo-Hoppa <hoppa.2@buckeyemail.osu.edu>
2025-04-22 13:50:00 -0400

    Update ShaftCalculations.m
    Final real values inputted into variables. Final minimum shaft diameters calculated

--- code/ShaftCalculations.m
+++ code/ShaftCalculations.m
@@ -15,9 +15,9 @@ S_e = S_u/2;            % Endurance limit (MPa)
 
 % Other Groups
 m_fw = 2.25;               % Flywheel Mass(kg)
-m_dg = 1;               % Drive Gear Mass
-m_pg = 1;               % Driven Gear Mass
-F_gear = 1;      %force transmitted by gears
+m_dg = 0.85;              % Drive Gear Mass
+m_pg = 1.066;               % Driven Gear Mass
+F_gear = 6.5359;      %force transmitted by gears
 GR = 1.1; %gear ratio
 
 
@@ -29,17 +29,17 @@ F_dg = m_dg * g + F_gear;        % Drive gear force (N)
 F_m = F_dg / 2;         % Vertical motor force (N) 
 F_b1 = F_dg / 2;        % Bearing force (N)
 
-L1 = .1; %distance from bearing to gear
+L1 = (37/2)/1000; %distance from bearing to gear (37/2)
 
 M1 = L1*F_b1; %moment at gear
 
-T1 = 12.0803*GR; %torque at gear
+T1 = 12.0803/GR; %torque at gear (speed reducer)
 
 A = sqrt(4*((Kf*M1)^2)+3*(Kfs*T1)^2); %at gear
 
 B = 0; %assumed no mean stresses
 
-dS1 = ((16*n/pi)*((A/(S_e*10^6))+(B/(S_u*10^6))))^(1/3);
+dS1 = 1000*((16*n/pi)*((A/(S_e*10^6))+(B/(S_u*10^6))))^(1/3);
 
 
 %% Shaft 2 (Driven gear, flywheel, coupling to crankshaft)
@@ -53,14 +53,12 @@ F_fw = m_fw * g;        % Flywheel weight (N)
 F_b2 = F_pg / 2;        % Bearing force 1 (N)
 F_b3 = (F_pg+F_fw) / 2; % Bearing force 2 (N)
 F_b4 = F_fw / 2;        % Bearing force 3 (N)
-T_pg = 0;               % Pinion gear torque (N*m)
-T_fw = 0;               % Flywheel torque (N*m)
-T_cp = 12.0803;              % Max Coupling torque (N*m)
+T_cp = 12.0803;         % Max Coupling torque (N*m)
 
-L1 = .05; %distance from bearing 1 to gear
-L2 = .05; %distance from gear to bearing 2
-L3 = .05; %distance from bearing 2 to flywheel
-L4 = .05; %distance from flywheel to bearing 3
+L1 = (37/2)/1000; %distance from bearing 1 to gear (37/2)
+L2 = (37/2)/1000; %distance from gear to bearing 2 (37/2)
+L3 = (40.113 + 5.888)/(2*1000); %distance from bearing 2 to flywheel (40.113 + 5.888)/2
+L4 = L3; %distance from flywheel to bearing 3 same
 
 M1 = L1*F_b2; %Bending moment at gear
 M2 = M1+L2*(F_pg+F_gear); %Bending moment at bearing 2
@@ -72,17 +70,17 @@ A3 = sqrt(4*((Kf*M3)^2)+3*(Kfs*T_cp)^2); %at flywheel
 
 B = 0; %assumed no mean stresses
 
-dS2_1 = ((16*n/pi)*((A1/(S_e*10^6))+(B/(S_u*10^6))))^(1/3);
-dS2_2 = ((16*n/pi)*((A2/(S_e*10^6))+(B/(S_u*10^6))))^(1/3);
-dS2_3 = ((16*n/pi)*((A3/(S_e*10^6))+(B/(S_u*10^6))))^(1/3);
+dS2_1 = 1000*((16*n/pi)*((A1/(S_e*10^6))+(B/(S_u*10^6))))^(1/3);
+dS2_2 = 1000*((16*n/pi)*((A2/(S_e*10^6))+(B/(S_u*10^6))))^(1/3);
+dS2_3 = 1000*((16*n/pi)*((A3/(S_e*10^6))+(B/(S_u*10^6))))^(1/3);
 
 %% Shaft 3 (Crank Shaft)
 Kf = 1.8;           %Fatigue Bending stress concentration factor for a crankshaft
 Kfs = 2.1;          %Fatigue torsional stress concentration for a crankshaft 
 
-LB = .1;             %distance between two ball bearings 
+LB = (32.694 +61.994)/(2*1000);             %distance between two ball bearings (32.694 +61.994)/2 mm
 LS = LB/2;          %distance between center of bearing and applied spring force
-LK = .01;             %distance from center of ball bearing to edge of crank
+LK = 14.525/1000;             %distance from center of ball bearing to edge of crank (14.525 mm)
 F_spr_min = 700;      %minimum spring force (connecting rod)
 F_spr_max = 1672;      %maximum spring force (connecting rod)
 F_bB_min = F_spr_min*-1; %minimum bearing force
@@ -111,10 +109,10 @@ B1 = sqrt(4*((Kf*Mm_edge)^2)+3*(Kfs*Tm)^2); %at edge of crank
 B2 = sqrt(4*((Mm_center)^2)+3*(Tm)^2); %at center of crank 
 
 %required minimum diameters to keep factor of safety according to goodman criterion
-dS3_1 = ((16*n/pi)*((A1/(S_e*10^6))+(B1/(S_u*10^6))))^(1/3); %Normal operation at edge of crank
-dS3_2 = ((16*n/pi)*((A2/(S_e*10^6))+(B1/(S_u*10^6))))^(1/3); %Worst case scenario at edge of crank
-dS3_3 = ((16*n/pi)*((A3/(S_e*10^6))+(B2/(S_u*10^6))))^(1/3); %Normal operation at center of crank
-dS3_4 = ((16*n/pi)*((A4/(S_e*10^6))+(B2/(S_u*10^6))))^(1/3); %worst case scenario at center of crank
+dS3_1 = 1000*((16*n/pi)*((A1/(S_e*10^6))+(B1/(S_u*10^6))))^(1/3); %Normal operation at edge of crank
+dS3_2 = 1000*((16*n/pi)*((A2/(S_e*10^6))+(B1/(S_u*10^6))))^(1/3); %Worst case scenario at edge of crank
+dS3_3 = 1000*((16*n/pi)*((A3/(S_e*10^6))+(B2/(S_u*10^6))))^(1/3); %Normal operation at center of crank
+dS3_4 = 1000*((16*n/pi)*((A4/(S_e*10^6))+(B2/(S_u*10^6))))^(1/3); %worst case scenario at center of crank
 
 
 
